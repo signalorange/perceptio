2808c7d1cf7606b8b5d17df2a1a161905d1fb2f3
Jacob Roy <jacob@signalorange.tech>
2024-09-17 09:56:33 -0400

    changement des couleurs

--- lib/live_dashboard_web/controllers/api_controller.ex
+++ lib/live_dashboard_web/controllers/api_controller.ex
@@ -3,10 +3,30 @@ defmodule LiveDashboardWeb.APIController do
 
   def nb_commandes_ln(conn, _params) do
     # Replace this with your logic to retrieve the value
-    query = "SELECT count(*) as count
-            from ELEC_IND_COMMANDES_LN WITH(NOLOCK)"
-    value = fetch_query(query)
-    json(conn, %{value: value})
+    query = "SELECT
+          (select
+              count(*) as count
+            from ve_livraisons_ln with(nolock)
+            --inner join ve_livraisons with(nolock) on ve_livraisons.id = ve_livraisons_ln.ve_livraisons_id
+            where  ve_livraisons_ln.created_date >= DATEADD(HOUR, 5, CAST(CAST(GETDATE() AS DATE) AS DATETIME))
+              and (
+					ve_livraisons_ln.created_by = 'EXPEDITION1'
+					or ve_livraisons_ln.created_by = 'EXPEDITION2'
+					or ve_livraisons_ln.created_by = 'EXPEDITION3'
+          or ve_livraisons_ln.created_by = 'VERIFICATION1'
+          or ve_livraisons_ln.created_by = 'VERIFICATION2'
+          or ve_livraisons_ln.created_by = 'SIMON.HOUMMAS'
+          or ve_livraisons_ln.created_by = 'DONALD.GUITARD'
+          or ve_livraisons_ln.created_by = 'MIGUEL.LACOMBE'
+					)) as livrees,
+          sum(iif(coalesce(statut,0) = 4,1,0)) as completees,
+          sum(iif(coalesce(statut,0) = 3,1,0)) as encours,
+          sum(iif(coalesce(statut,0) = 2,1,0)) as imprimees,
+          sum(iif(coalesce(statut,0) = 1,1,0)) as afaire
+        FROM ELEC_IND_COMMANDES_LN with(nolock)"
+    value = fetch_query_total(query)
+    json_data = convert_tds_result_to_json(value)
+    json(conn, json_data)
   end
 
   def nb_commandes_ln_types(conn, _params) do
@@ -110,6 +130,11 @@ defmodule LiveDashboardWeb.APIController do
 					ve_livraisons_ln.created_by = 'EXPEDITION1'
 					or ve_livraisons_ln.created_by = 'EXPEDITION2'
 					or ve_livraisons_ln.created_by = 'EXPEDITION3'
+          or ve_livraisons_ln.created_by = 'VERIFICATION1'
+          or ve_livraisons_ln.created_by = 'VERIFICATION2'
+          or ve_livraisons_ln.created_by = 'SIMON.HOUMMAS'
+          or ve_livraisons_ln.created_by = 'DONALD.GUITARD'
+          or ve_livraisons_ln.created_by = 'MIGUEL.LACOMBE'
 					)"
     value = fetch_query(query)
     json(conn, %{value: value})
