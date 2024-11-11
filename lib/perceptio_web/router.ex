46f3a380f0d571f5ee02ea23857a242cb17e0b4c
Jacob Roy <jacob@signalorange.tech>
2024-11-11 12:29:24 -0500

    enable dashboard

--- lib/perceptio_web/router.ex
+++ lib/perceptio_web/router.ex
@@ -26,19 +26,21 @@ defmodule PerceptioWeb.Router do
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  # import Phoenix.LiveDashboard.Router
+  #if Application.compile_env(:perceptio, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
 
-  # scope "/dev" do
-  #   pipe_through :browser
+    scope "/dev" do
+      pipe_through :browser
 
-  #   perceptio "/dashboard", metrics: PerceptioWeb.Telemetry
-  #   #forward "/mailbox", Plug.Swoosh.MailboxPreview
-  # end
+      live_dashboard "/dashboard", metrics: PerceptioWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  #end
 end
 
 defmodule PerceptioWeb.RedirectController do
